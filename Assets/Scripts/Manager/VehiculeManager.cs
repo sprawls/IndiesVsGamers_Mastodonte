ad8da8ae62023d9e8958a0a49ed1904f25df98db
Antoine Brouillette <antoineqc@hotmail.com>
2015-07-17 06:57:30 -0400

    Car basic controls

---
+++ Assets/Scripts/Manager/VehiculeManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehiculeManager : MonoBehaviour {
+
+    [Header("Vehicule Stats")]
+    public float forwardSpeed;
+    public float horizontalSpeed;
+
+    void Update () {
+        GoForward();
+        UpdateHorizontalPos(Input.GetAxis("Horizontal"));
+    }
+
+    void GoForward() {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x,
+                                                    gameObject.transform.position.y,
+                                                    gameObject.transform.position.z + forwardSpeed * Time.deltaTime);
+    }
+
+    void UpdateHorizontalPos(float inputMultiplier) {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x + horizontalSpeed * Time.deltaTime * inputMultiplier,
+                                                    gameObject.transform.position.y,
+                                                    gameObject.transform.position.z);
+    }
+}
