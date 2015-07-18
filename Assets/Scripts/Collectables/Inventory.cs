32a16ced61181d01b0a823101fde015143e778e3
Antoine Brouillette <antoineqc@hotmail.com>
2015-07-18 17:46:35 -0400

    Collectable Start

---
+++ Assets/Scripts/Collectables/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+    public List<Collectable> collectables = new List<Collectable>();
+
+    public void AddToInventory(Collectable collectable) {
+        collectables.Add(collectable);
+    }
+}
