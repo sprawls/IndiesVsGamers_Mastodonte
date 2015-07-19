14dc1b4dc4ed49dc99fc816fa83ecd1343075ba9
sprawls <alex3521@hotmail.fr>
2015-07-19 14:15:10 -0400

    ADded Wipers

---
+++ Assets/wipe.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wipe : MonoBehaviour {
+
+    public void Wipe() {
+        StartCoroutine(WipeCoroutine());
+    }
+
+    IEnumerator WipeCoroutine() {
+        Quaternion s = Quaternion.identity;
+        Quaternion e = Quaternion.Euler(0,0,90);
+
+        for (float i = 0f; i < 1f; i += Time.deltaTime / 0.6f) {
+            transform.localRotation = Quaternion.Slerp(s, e, i);
+            yield return null;
+        }
+
+        for (float i = 0f; i < 1f; i += Time.deltaTime / 0.6f) {
+            transform.localRotation = Quaternion.Slerp(e, s, i);
+            yield return null;
+        }
+
+        transform.localRotation = s;
+    }
+}
