13f21876c971bdc40c7d9b985f056cb3cebc0175
sprawls <alex3521@hotmail.fr>
2015-07-18 23:29:26 -0400

    Added Intro Scene

---
+++ Assets/Scripts/Utility/GoToNextScene.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToNextScene : MonoBehaviour {
+
+    public float idleTimeToSkip = 15f;
+    public float curIdleTime = 0f;
+    public float holdTimeToSkip = 2f;
+    public float curHoldTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0)) {
+            curHoldTime += Time.deltaTime;
+            if (curHoldTime > holdTimeToSkip) NextScene();
+        } else {
+            curHoldTime = 0;
+        }
+
+        curIdleTime += Time.deltaTime;
+        if (curIdleTime > idleTimeToSkip) NextScene();
+       
+	}
+
+
+
+
+    void NextScene() {
+        Application.LoadLevel("Phase1");
+    }
+}
