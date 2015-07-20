1e6467b5e53bf2083c3378c52b08e261a5a7eabe
sprawls <alex3521@hotmail.fr>
2015-07-19 21:50:24 -0400

    Added Particles effects to weapon grab

--- Assets/Scripts/Collectables/Collectable.cs
+++ Assets/Scripts/Collectables/Collectable.cs
@@ -5,10 +5,12 @@ public class Collectable : MonoBehaviour {
     
     public int pointsGiven;
     public int amtAmmo;
+    public GameObject scoreParticles;
 
     public void OnPickUp() {
-        GameManager.instance.scoreSystem.AddScore(pointsGiven,gameObject, new Vector3(0,1,0), false);
+        GameManager.instance.addScore(pointsGiven);
         GameManager.instance.inventory.currentAmmo += amtAmmo;
+        GameObject go = (GameObject)Instantiate(scoreParticles, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
