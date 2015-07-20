471a3da09741cbcbc052ee43b9bd282c41870059
Antoine Brouillette <antoineqc@hotmail.com>
2015-07-19 22:45:29 -0400

    Spawner and fix end game UI

--- Assets/Scripts/Collectables/Collectable.cs
+++ Assets/Scripts/Collectables/Collectable.cs
@@ -8,7 +8,7 @@ public class Collectable : MonoBehaviour {
     public GameObject scoreParticles;
 
     public void OnPickUp() {
-        GameManager.instance.addScore(pointsGiven);
+        GameManager.instance.scoreSystem.AddScore(pointsGiven, gameObject, new Vector3(0,3,0));
         GameManager.instance.inventory.currentAmmo += amtAmmo;
         GameObject go = (GameObject)Instantiate(scoreParticles, transform.position, Quaternion.identity);
         Destroy(gameObject);
